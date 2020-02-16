f1f663f1960a24640c21ff65e773ffd35ce5df47
saya-nel <pablobellotw@gmail.com>
2020-02-16 16:48:27 +0100

    Ajout Obytelib

---
+++ run.sh
@@ -0,0 +1,1 @@
+dune build src/bytecodeParser.exe && ./_build/default/src/bytecodeParser.exe samples/test1.cmo && dune clean
---
+++ src/bytecodeParser.ml
@@ -0,0 +1,21 @@
+open OByteLib
+
+let usage () =
+  Printf.eprintf "Usage: %s <input.byte>\n" Sys.argv.(0);
+  exit 1
+
+let inpath = match Sys.argv with
+  | [| _; inpath |] -> inpath
+  | _ -> usage ()
+
+let () =
+  match Filename.extension inpath with
+  | ".byte" ->
+    let bytefile = Bytefile.read inpath in (* Load the given bytecode file *)
+    Bytefile.print stdout bytefile;        (* Pretty-print its contents    *)
+    Interp.eval_bytefile bytefile;         (* Evaluate its code            *)
+  | ".cmo" ->
+    let cmofile = Cmofile.read inpath in   (* Load the given .cmo file     *)
+    Cmofile.print stdout cmofile           (* Pretty-print its contents    *)
+  | _ ->
+    usage ()
