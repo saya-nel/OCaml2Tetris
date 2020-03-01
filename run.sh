0bebb8e7973682b7ff2e7be54c8c5fb055a14af0
saya-nel <pablobellotw@gmail.com>
2020-03-01 15:40:07 +0100

    Correction bug run.sh

--- run.sh
+++ run.sh
@@ -1,12 +1,15 @@
+#!/bin/bash
+
 # Build executable
 dune build src/bytecodeParser.exe 
 # Run executable with ocamlc -dinstr return
 cd samples
-ocamlc -dinstr $@ | ../_build/default/src/bytecodeParser.exe 
+ocamlc -dinstr $@ &> res.txt 
+cat res.txt | ../_build/default/src/bytecodeParser.exe
 cd .. 
 # Clean folders
 dune clean 
 rm samples/*.cmi 
 rm samples/*.cmo 
 rm samples/a.out
-
+rm samples/res.txt
