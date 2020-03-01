1d785e88ca9a4584c3e6703e55fd205b24a19ed1
saya-nel <pablobellotw@gmail.com>
2020-03-01 13:31:49 +0100

    bis.

--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
-dune build src/bytecodeParser.exe && cd samples && ocamlc -dinstr $@ | ../_build/default/src/bytecodeParser.exe && cd .. && dune clean && rm samples/*.cmi && rm samples/*.cmo && rm samples/a.out
+# Build executable
+dune build src/bytecodeParser.exe 
+# Run executable with ocamlc -dinstr return
+cd samples
+ocamlc -dinstr $@ | ../_build/default/src/bytecodeParser.exe 
+cd .. 
+# Clean folders
+dune clean 
+rm samples/*.cmi 
+rm samples/*.cmo 
+rm samples/a.out
 
