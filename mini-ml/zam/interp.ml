46ff6816beda283e4cdb133d740aee0087614021
Loïc Sylvestre <loic.sylvestre@outlook.fr>
2020-03-16 22:07:15 +0100

    instructions accn et pushaccn

--- mini-ml/zam/interp.ml
+++ mini-ml/zam/interp.ml
@@ -20,16 +20,42 @@ let interp () =
   while !pc >= 0 do
     debug_print_state ();
     match code.(!pc) with
-    | 8 (* Acc *) -> 
+    | 0 (* ACC0 *) -> acc := stack.(0); incr pc
+    | 1 (* ACC1 *) -> acc := stack.(1); incr pc
+    | 2 (* ACC2 *) -> acc := stack.(2); incr pc
+    | 3 (* ACC3 *) -> acc := stack.(3); incr pc    
+    | 4 (* ACC4 *) -> acc := stack.(4); incr pc
+    | 5 (* ACC5 *) -> acc := stack.(5); incr pc
+    | 6 (* ACC6 *) -> acc := stack.(6); incr pc
+    | 7 (* ACC7 *) -> acc := stack.(7); incr pc   
+    | 8 (* ACC *) -> 
        incr pc;
-       let n = code.(!pc) in 
+       let n = code.(!pc) in
        if n >= Array.length stack || n < 0 then failwith "stack pleine"
        else acc := stack.(n);
        incr pc
     | 9 (* Push *) -> 
-       incr sp;
-       stack.(!sp) <- !acc;
-       incr pc
+      incr sp; stack.(!sp) <- !acc; incr pc
+    | 10 (* PUSHACC0 (~ PUSH) *) -> 
+      incr sp; stack.(!sp) <- !acc; incr pc
+    | 11 (* PUSHACC1 *) -> 
+      incr sp; stack.(!sp) <- !acc; acc := stack.(1); incr pc
+    | 12 (* PUSHACC2 *) -> 
+      incr sp; stack.(!sp) <- !acc; acc := stack.(2); incr pc
+    | 13 (* PUSHACC3 *) -> 
+      incr sp; stack.(!sp) <- !acc; acc := stack.(3); incr pc
+    | 14 (* PUSHACC4 *) -> 
+      incr sp; stack.(!sp) <- !acc; acc := stack.(4); incr pc
+    | 15 (* PUSHACC5 *) -> 
+      incr sp; stack.(!sp) <- !acc; acc := stack.(5); incr pc
+    | 16 (* PUSHACC6 *) -> 
+      incr sp; stack.(!sp) <- !acc; acc := stack.(6); incr pc
+    | 17 (* PUSHACC7 *) -> 
+      incr sp; stack.(!sp) <- !acc; acc := stack.(7); incr pc
+    | 18 (* PUSHACC *) -> 
+      incr pc; 
+      let n = code.(!pc) in
+      incr sp; stack.(!sp) <- !acc; acc := stack.(n); incr pc
     | 103 (* ConstInt *) -> 
        incr pc;
        let n = code.(!pc) in
