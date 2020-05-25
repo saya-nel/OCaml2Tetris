dd02085152c44a8179d319fb889676b32c1a7c67
Loïc Sylvestre <loic.sylvestre@outlook.fr>
2020-05-25 13:01:34 +0200

    refactor prims.ml

--- vm/zam-miniML/src/prims.ml
+++ vm/zam-miniML/src/prims.ml
@@ -1,19 +1,12 @@
-(************** module prims.ml ****************)
-(*** hypothèses :                            ***)
-(***  - `(long_val n) = (val_long n)`.       ***)
-(***  toute valeur >= (-16384) est un entier ***)
-(***********************************************)
-
 let isint v = 
   if Mlvalues.is_ptr v then 0 else 1
 
 (*** opérations arithmétiques ***)
 
-let negint v = (-v)
-let addint v1 v2 = v1 + v2 
-let subint v1 v2 = v1 - v2
-let mulint v1 v2 = (v1 * v2)
-
+let negint n = (-n)
+let addint n1 n2 = n1 + n2 
+let subint n1 n2 = n1 - n2
+let mulint n1 n2 = (n1 * n2)
 
 (*** division d'après la doc OCaml : (-x) / y = x / (-y) = -(x / y). ***)
 (*** (/) 17 2       ~>  8  *)
@@ -21,16 +14,16 @@ let mulint v1 v2 = (v1 * v2)
 (*** (/) (-17) 2    ~> -8  *)
 (*** (/) (-17) (-2) ~>  8  *)
 
-let rec div_aux v1 v2 acc = 
-  if v1 < v2 then acc
-  else div_aux (v1 - v2) v2 (acc + 1)
+let rec div_aux n1 n2 accu = 
+  if n1 < n2 then accu
+  else div_aux (n1 - n2) n2 (accu + 1)
 
-let div v1 v2 = div_aux v1 v2 0
+let div n1 n2 = div_aux n1 n2 0
 
-let divint v1 v2 =
-  if v2 = 0 then failwith "divint" else
-    if v1 >= 0 then (if v2 >= 0 then div v1 v2 else - (div v1 (- v2)))
-    else (if v2 >= 0 then - (div (- v1) v2) else (div (- v1) (- v2)))
+let divint n1 n2 =
+  if n2 = 0 then failwith "divint" else
+    if n1 >= 0 then (if n2 >= 0 then div n1 n2 else - (div n1 (- n2)))
+    else (if n2 >= 0 then - (div (- n1) n2) else (div (- n1) (- n2)))
 
 (*** modulo d'après la doc OCaml : `((mod) x y) < 0` si et seulement si `x < 0` ***)
 (*** (mod) 11 3        ~>  2 *)
@@ -38,76 +31,76 @@ let divint v1 v2 =
 (*** (mod) (-11) 3     ~> -2 *)
 (*** (mod) (-11) (-3)  ~> -2 *)
 
-let rec modulo v1 v2 =
-  if v1 < v2 then v1
-  else modulo (v1 - v2) v2
+let rec modulo n1 n2 =
+  if n1 < n2 then n1
+  else modulo (n1 - n2) n2
 
-let rec modint v1 v2 = (* dans un premier temps, on suppose que v2 >= 0 ***)
-  if v2 = 0 then failwith "modint" else
-    if v1 < 0 then - (modulo (- v1) (abs v2)) else (modulo v1 (abs v2))
+let rec modint n1 n2 = (* dans un premier temps, on suppose que n2 >= 0 ***)
+  if n2 = 0 then failwith "modint" else
+    if n1 < 0 then - (modulo (- n1) (abs n2)) else (modulo n1 (abs n2))
 
 
 (*** opérations logiques ***)
 
-let andint v1 v2 = 
-  if v1 <> 0 && v2 <> 0 then 1 else 0
+let andint n1 n2 = 
+  if n1 <> 0 && n2 <> 0 then 1 else 0
 
-let orint v1 v2 = 
-  if v1 <> 0 || v2 <> 0 then 1 else 0
+let orint n1 n2 = 
+  if n1 <> 0 || n2 <> 0 then 1 else 0
 
-let xorint v1 v2 = 
-  if (v1 <> 0 && v2 = 0) || (v1 <> 0 && v2 = 0) then 1 else 0
+let xorint n1 n2 = 
+  if (n1 <> 0 && n2 = 0) || (n1 <> 0 && n2 = 0) then 1 else 0
 
-let bnot v = 
-  if v = 0 then 1 else 0
+let bnot n = 
+  if n = 0 then 1 else 0
 
 (*** opérations de décalage ***)
 (*** dans un premier temps, on suppose que le déplacement est toujours >= 0 ***)
 (*** dans un premier temps, on ne fait pas de distinction entre lsr et asr  ***)
-let rec lslint v dep =
-  if dep = 0 then v
-  else lslint (v + v) (dep - 1)
+let rec lslint n dep =
+  if dep = 0 then n
+  else lslint (n + n) (dep - 1)
 
-let rec lsrint v dep =
-  if dep = 0 then v
-  else lsrint (v / 2) (dep - 1)
+let rec lsrint n dep =
+  if dep = 0 then n
+  else lsrint (n / 2) (dep - 1)
 
-let asrint v1 v2 = lsrint v1 v2 
+let asrint n1 n2 = lsrint n1 n2 
 
 (*** opérations de comparaison ***)
 
 (*** égalité physique ***)
-let eq v1 v2 = 
-  if v1 = v2 then 1 else 0
+let eq n1 n2 = 
+  if n1 = n2 then 1 else 0
 
 (*** différence physique ***)
-let neq v1 v2 = 
-  if v1 <> v2 then 1 else 0
+let neq n1 n2 = 
+  if n1 <> n2 then 1 else 0
 
-let ltint v1 v2 = 
-  if v1 < v2 then 1 else 0
+let ltint n1 n2 = 
+  if n1 < n2 then 1 else 0
 
-let leint v1 v2 = 
-  if v1 <= v2 then 1 else 0
+let leint n1 n2 = 
+  if n1 <= n2 then 1 else 0
 
-let gtint v1 v2 = 
-  if v1 > v2 then 1 else 0
+let gtint n1 n2 = 
+  if n1 > n2 then 1 else 0
 
-let geint v1 v2 = 
-  if v1 >= v2 then 1 else 0
+let geint n1 n2 = 
+  if n1 >= n2 then 1 else 0
 
-let compare_imm v1 v2 =
-  if v1 < v2 then -1 else if v1 > v2 then 1 else 0
+let compare_imm n1 n2 =
+  if n1 < n2 then -1 else if n1 > n2 then 1 else 0
 
 (*** comparaison (<) non signée               ***)
-(*** v1 < 0 && v2 >= 0 => (ultint v1 v2) ~> 0 ***)
-let ultint v1 v2 =
-  if v1 < 0 then (if v2 < 0 then gtint v1 v2 else 0)
-  else if v2 < 0 then 0 else ltint v1 v2
+(*** n1 < 0 && n2 >= 0 => (ultint n1 n2) ~> 0 ***)
+let ultint n1 n2 =
+  if n1 < 0 then (if n2 < 0 then gtint n1 n2 else 0)
+  else if n2 < 0 then 0 else ltint n1 n2
 
 (*** comparaison (>=) non signée              ***)
-(*** v1 < 0 && v2 >= 0 => (ugeint v1 v2) ~> 1 ***)
-let ugeint v1 v2 =
-  if v1 < 0 then (if v2 < 0 then leint v1 v2 else 1)  
-  else if v2 < 0 then 1 else geint v1 v2
+(*** n1 < 0 && n2 >= 0 => (ugeint n1 n2) ~> 1 ***)
+let ugeint n1 n2 =
+  if n1 < 0 then (if n2 < 0 then leint n1 n2 else 1)  
+  else if n2 < 0 then 1 else geint n1 n2
 
